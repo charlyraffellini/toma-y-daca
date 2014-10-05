2be6f8ff69cfd2f3a9e81ea87fbc81e8cae03e25
PalumboN <nahuel.palumbo@gmail.com>
2014-10-05 20:25:44 -0300

    Los Trade saben ser aceptados y validarse

--- src/main/java/models/domain/TradeRequest.java
+++ src/main/java/models/domain/TradeRequest.java
@@ -1,5 +1,7 @@
 package models.domain;
 
+import models.domain.exceptions.TradeDoesntBelongToUser;
+
 /**
  * Created by Palumbo on 29/09/2014.
  */
@@ -10,5 +12,25 @@ public class TradeRequest extends DomainObject{
     public TradeRequest(UserWithItem sender, UserWithItem receiver) {
         this.sender = sender;
         this.receiver = receiver;
+    }
+
+    public void accept() {
+        this.validate();
+        this.changeItems();
+    }
+
+    private void changeItems() {
+        this.sender.item.changeOwnerTo(this.receiver.user);
+        this.receiver.item.changeOwnerTo(this.sender.user);
+    }
+
+    private void validate() {
+        this.sender.validate();
+        this.receiver.validate();
+    }
+
+    public void validateOwner(User user) {
+        if (this.receiver.user != user)
+            throw new TradeDoesntBelongToUser(this, user);
     }
 }
